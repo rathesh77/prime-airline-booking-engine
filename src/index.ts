525d333ab938f1960c783a6dafe224c8b56d04a1
flefur <f.lefur@djust.io>
2023-09-15 16:46:24 +0200

    unify health checks

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 
 app.use(bookRouter);
 
-app.get('/healthcheck', (_, res: Response) => {
-  res.send('alive');
-});
+app.get('/health', (_, res: Response) => {
+    res.sendStatus(200)
+})
 
 app.listen(port, function () {
   console.log(`App is listening on port ${port} !`);
